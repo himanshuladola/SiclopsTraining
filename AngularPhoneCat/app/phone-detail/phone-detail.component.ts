7a96f7ac6cd47aef4eb79479d830bd6b79b6ebfa
Himanshu Ladola <Himanshu.Ladola@oneadvanced.com>
2016-12-29 14:11:02 +0530

    Initial commit

---
+++ AngularPhoneCat/app/phone-detail/phone-detail.component.ts
@@ -0,0 +1,27 @@
+'use strict';
+
+class PhoneDetailController {
+    phone: any;
+    mainImageUrl: string;
+
+    static $inject = ['$routeParams', 'Phone'];
+    constructor($routeParams: angular.route.IRouteParamsService,
+        Phone: any) {
+        let phoneId = $routeParams["phoneId"];
+        this.phone = Phone.get({ phoneId },
+            (phone: any) => {
+                this.setImage(phone.images[0]);
+            });
+    }
+
+    setImage(imageurl: string) {
+        this.mainImageUrl = imageurl;
+    }
+}
+// Register `phoneDetail` component, along with its associated controller and template
+angular.
+  module('phoneDetail').
+  component('phoneDetail', {
+    templateUrl: 'phone-detail/phone-detail.template.html',
+    controller: PhoneDetailController
+  });
---
+++ AngularPhoneCat/typings/index.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="globals/core-js/index.d.ts" />
+/// <reference path="globals/es6-collections/index.d.ts" />
+/// <reference path="globals/es6-promise/index.d.ts" />
